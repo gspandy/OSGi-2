370845bc01ca31db727fa72191f6ab65315f2446
jetgeng <jetgeng@gmail.com>
2011-07-06 00:24:05 +0800

    初始化gemini系列blog的实现文档

---
+++ org.gunn.gemini.demo/src/org/gunn/gemini/demo/Activator.java
@@ -0,0 +1,35 @@
+package org.gunn.gemini.demo;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+	private Logger logger = LoggerFactory.getLogger(Activator.class);
+	
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+	    logger.info("start the bundle");
+		Activator.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+	    logger.info("stop the bundle");
+		Activator.context = null;
+	}
+
+}
---
+++ org.gunn.gemini.demo/src/org/gunn/gemini/demo/POJOWillInContainer.java
@@ -0,0 +1,36 @@
+package org.gunn.gemini.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+/**
+ * This pojo will create by blueprint container
+ * @author 
+ *
+ */
+public class POJOWillInContainer {
+    private Logger logger = LoggerFactory.getLogger(POJOWillInContainer.class);
+    
+    private String name ;
+    
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        logger.info("the new name is:" + name);
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        logger.info("the new age value is:" + age);
+        this.age = age;
+    }
+    
+    
+}
