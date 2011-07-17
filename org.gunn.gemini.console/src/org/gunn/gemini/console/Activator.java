75b7bc35166a7ab168aa9e808294bd3117c42fc6
jetgeng <jetgeng@gmail.com>
2011-07-18 00:16:39 +0800

    通过命令来执行动作。

--- org.gunn.gemini.console/src/org/gunn/gemini/console/Activator.java
+++ org.gunn.gemini.console/src/org/gunn/gemini/console/Activator.java
@@ -1,7 +1,10 @@
 package org.gunn.gemini.console;
 
+import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
+import org.eclipse.osgi.framework.console.CommandInterpreter;
 import org.eclipse.osgi.framework.console.CommandProvider;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
@@ -13,12 +16,24 @@ public class Activator extends AbstractBlueprintActivator  implements CommandPro
 	private BeanTestEngine engine;
 	private Map<String , Object> scriptParam;
 	
+	
+	public Activator(){
+		scriptParam = new HashMap<String, Object>();
+		scriptParam.put("act", this);
+	}
+	
+	
 	/*
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
 		super.start(bundleContext);
+		
+		engine = new BeanTestEngine();
+		engine.init();
+		
+		getContext().registerService(CommandProvider.class.getName(), this, null);
 	}
 
 	/*
@@ -35,6 +50,34 @@ public class Activator extends AbstractBlueprintActivator  implements CommandPro
 		return helptips;
 	}
 	
+	
+	/**
+	 * 通过Groovy代码来查看bean的内容。
+	 * @param interpreter
+	 * @return
+	 */
+	public Object _invokeGV( CommandInterpreter interpreter){
+		StringBuffer code = new StringBuffer();
+		String temp = interpreter.nextArgument();
+		while( StringUtils.isNotBlank( temp ) ){
+			code.append(temp).append(" ");
+			temp = interpreter.nextArgument();
+		}
+		
+		System.out.println( engine.run(getCommandCode(code.toString()), "adapter", getParam()) );
+		return null;
+	}
+	
+	private String getCommandCode(String arg){
+		StringBuffer strBuff = new StringBuffer(" def adapter(){").append("\n\r");
+		strBuff.append(arg);
+		strBuff.append("}");
+		return strBuff.toString();
+	}
+	
+	private Map<String,Object> getParam(){
+		return scriptParam;
+	}
 	
 
 }
